8f46c0e5a29ac66c10d80e02e445aaca9afc432b
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-20 22:04:30 +0530

    Refactor: To create generics= class to take 3 variables of generic type

--- src/com/bridgelabz/generics/MaximumTestGenerics.java
+++ src/com/bridgelabz/generics/MaximumTestGenerics.java
@@ -1,28 +1,38 @@
 package com.bridgelabz.generics;
 
 public class MaximumTestGenerics<T> {
+
+	/**
+	 * here used Generic functions. here T is the type of data. Gradually it will
+	 * return the maximum.
+	 */
 	T num1, num2, num3;
 
-	/*
-	 * Given three String values then used compare to method
-	 */
+	public MaximumTestGenerics(T num1, T num2, T num3) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+	}
 
-	public static String maximumString(String num1, String num2, String num3) {
+	public static <T extends Comparable<T>> T MaximumAmongThree(T num1, T num2, T num3) {
 
-		String maximumString = num1;
+		T maximum = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-			maximumString = num1;
+			maximum = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
-			maximumString = num2;
+			maximum = num2;
 		else
-			maximumString = num3;
-		return maximumString;
+			maximum = num3;
+		return maximum;
 
 	}
 
 	// print maximum Float numbers
 	public static void main(String[] args) {
-		System.out.println("Maximum String Among 3 will be :- " + maximumString("Papaya", "Apple", "Mango"));
+		System.out.println("The max Number Among Three String numbers is:-\n "
+				+ MaximumAmongThree("Orange", "Dragonfruit", "Apple"));
+		System.out.println("The max Number Among Three Integer numbers is:-\n " + MaximumAmongThree(14, 56, 27));
+		System.out.println("The max Number Among Three Float numbers is:-\n " + MaximumAmongThree(39.6f, 78.7f, 99.8f));
 	}
 
 }
