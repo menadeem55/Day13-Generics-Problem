214544894b3fbf7121aae089c1fe114806aa50cb
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-20 21:48:05 +0530

    Added: To getting the Float Maximum Number

--- src/com/bridgelabz/generics/MaximumTestGenerics.java
+++ src/com/bridgelabz/generics/MaximumTestGenerics.java
@@ -4,12 +4,12 @@ public class MaximumTestGenerics<T> {
 	T num1, num2, num3;
 
 	/*
-	 * Here i have used Generic functions. T is the type of data. then used compare
-	 * to method
+	 * Given three Floats and find maximum. First i have taken number one is
+	 * maximumNumber then used compare to method
 	 */
-	public static <T extends Comparable<T>> T maximumAmongThree(T num1, T num2, T num3) {
+	public static Float maximumFloat(Float num1, Float num2, Float num3) {
 
-		T maximumNumber = num1;
+		Float maximumNumber = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			maximumNumber = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
@@ -17,13 +17,12 @@ public class MaximumTestGenerics<T> {
 		else
 			maximumNumber = num3;
 		return maximumNumber;
-	}
-	// print maximum of three methods
 
-	public static void main(String[] args) {
-		System.out.println(
-				"The max Number Among Three String numbers is:-\n " + maximumAmongThree("Papaya", "Apple", "Mango"));
-		System.out.println("The max Number Among Three Integer numbers is:-\n " + maximumAmongThree(70, 40, 50));
-		System.out.println("The max Number Among Three Float numbers is:-\n " + maximumAmongThree(70.6f, 40.7f, 50.8f));
 	}
+
+	// print maximum Float numbers
+	public static void main(String[] args) {
+		System.out.println("Maximum Float Number Among 3 will be :- " + maximumFloat(70.6f, 40.7f, 50.8f));
+	}
+
 }
