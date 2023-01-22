053019a40c3506943ab8c68c48a0ef685b8d41a5
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-22 16:59:10 +0530

    Added: More than four Parameters

--- src/com/bridgelabz/generics/MaximumTestGenerics.java
+++ src/com/bridgelabz/generics/MaximumTestGenerics.java
@@ -6,33 +6,43 @@ public class MaximumTestGenerics<T> {
 	 * here used Generic functions. here T is the type of data. Gradually it will
 	 * return the maximum.
 	 */
-	T num1, num2, num3;
+	T num1, num2, num3, num4;
 
-	public MaximumTestGenerics(T num1, T num2, T num3) {
+	// Here this one is parameterised constructor having these parameters to initiate the values.
+
+	public MaximumTestGenerics(T num1, T num2, T num3, T num4) {
 		this.num1 = num1;
 		this.num2 = num2;
 		this.num3 = num3;
-	}
+		this.num4 = num4;
 
-	public static <T extends Comparable<T>> T MaximumAmongThree(T num1, T num2, T num3) {
+	}
+	/*
+	 * In this method it will check the values And give the maximum as an output.
+	 */
+
+	public static <T extends Comparable<T>> T maximumAmongFour(T num1, T num2, T num3, T num4) {
 
 		T maximum = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			maximum = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
 			maximum = num2;
-		else
+		else if (num3.compareTo(num4) > 0)
 			maximum = num3;
+		else
+			maximum = num4;
 		return maximum;
 
 	}
 
 	// print maximum Float numbers
 	public static void main(String[] args) {
-		System.out.println("The max Number Among Three String numbers is:-\n "
-				+ MaximumAmongThree("Orange", "Dragonfruit", "Apple"));
-		System.out.println("The max Number Among Three Integer numbers is:-\n " + MaximumAmongThree(14, 56, 27));
-		System.out.println("The max Number Among Three Float numbers is:-\n " + MaximumAmongThree(39.6f, 78.7f, 99.8f));
+		System.out.println("The max Number Among Four String numbers is:-\n "
+				+ maximumAmongFour("Orange", "Dragonfruit", "Apple", "Mango"));
+		System.out.println("The max Number Among Four Integer numbers is:-\n " + maximumAmongFour(14, 56, 27, 40));
+		System.out.println(
+				"The max Number Among Four Float numbers is:-\n " + maximumAmongFour(39.6f, 78.7f, 99.8f, 53.1f));
 	}
 
 }
