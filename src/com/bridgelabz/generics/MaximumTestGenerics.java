a4ce0b7e693966bcd8fc75b424590d264bf5e56e
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-22 17:03:57 +0530

    Added: Extend Method of Generic

--- src/com/bridgelabz/generics/MaximumTestGenerics.java
+++ src/com/bridgelabz/generics/MaximumTestGenerics.java
@@ -2,9 +2,8 @@ package com.bridgelabz.generics;
 
 public class MaximumTestGenerics<T> {
 
-	/**
-	 * here used Generic functions. here T is the type of data. Gradually it will
-	 * return the maximum.
+	/*
+	 * here used Generic functions. here T is the type of data. 
 	 */
 	T num1, num2, num3, num4;
 
@@ -36,13 +35,17 @@ public class MaximumTestGenerics<T> {
 
 	}
 
-	// print maximum Float numbers
+	/*
+	 * Extend the max method
+	 * Also print max to std out using Generic method.
+	 */
+	public static <E> void printMaximum(E num1, E num2, E num3, E num4, E max) {
+		System.out.println(max);
+	}
 	public static void main(String[] args) {
-		System.out.println("The max Number Among Four String numbers is:-\n "
-				+ maximumAmongFour("Orange", "Dragonfruit", "Apple", "Mango"));
-		System.out.println("The max Number Among Four Integer numbers is:-\n " + maximumAmongFour(14, 56, 27, 40));
-		System.out.println(
-				"The max Number Among Four Float numbers is:-\n " + maximumAmongFour(39.6f, 78.7f, 99.8f, 53.1f));
+		System.out.println("The max Number Among Four String numbers is:-\n "+maximumAmongFour("Orange", "Dragonfruit", "Apple", "Mango"));
+		System.out.println("The max Number Among Four Integer numbers is:-\n "+maximumAmongFour(14, 56, 27, 40));
+		System.out.println("The max Number Among Four Float numbers is:-\n "+maximumAmongFour(39.6f, 78.7f, 99.8f, 53.1f));
 	}
 
 }
